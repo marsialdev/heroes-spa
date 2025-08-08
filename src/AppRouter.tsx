92f1fae6196f87fbb5d74966db2420b071f5548f
marsialcode <mariomarciallp@gmail.com>
2025-08-07 20:08:30 -0600

    AppRouter Config

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,26 +1,30 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
 import { MarvelPage, DCPage, SearchPage, HeroPage } from './heroes/pages';
 import { LoginPage } from './auth/pages/LoginPage';
-import { HeroesLayout } from './heroes/layout';
-import { AuthLayout } from './auth/layout';
+import { ProtectedLayout, PublicLayout } from './layouts';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<HeroesLayout />}>
-          <Route index path="marvel" element={<MarvelPage />} />
+        <Route path="/" element={<ProtectedLayout />}>
+          {/* Redirect from / to /marvel */}
+          <Route index element={<Navigate to="/marvel" />} />
+
+          {/* Pages */}
+          <Route path="marvel" element={<MarvelPage />} />
           <Route path="dc" element={<DCPage />} />
+          <Route path="hero/:id" element={<HeroPage />} />
           <Route path="search" element={<SearchPage />} />
-          <Route path="hero" element={<HeroPage />} />
         </Route>
 
-        <Route element={<AuthLayout />}>
-          <Route path="login" element={<LoginPage />}></Route>
+        {/* Auth routes */}
+        <Route element={<PublicLayout />}>
+          <Route path="login" element={<LoginPage />} />
         </Route>
 
-        <Route path="/" element={<Navigate to={'/marvel'} />} />
-        <Route path="*" element={<Navigate to={'/login'} />} />
+        {/* Catch-all for invalid routes */}
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
   );
