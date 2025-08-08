f3a4d0b80af92ca467f637a8df2dd11062e309da
marsialcode <mariomarciallp@gmail.com>
2025-08-07 20:09:32 -0600

    HeroesApp main component, AuthProvider & AppRouter added

--- src/HeroesApp.tsx
+++ src/HeroesApp.tsx
@@ -1,9 +1,10 @@
 import { AppRouter } from './AppRouter';
+import { AuthProvider } from './auth/context';
 
 export const HeroesApp = () => {
   return (
-    <>
+    <AuthProvider>
       <AppRouter />
-    </>
+    </AuthProvider>
   );
 };
